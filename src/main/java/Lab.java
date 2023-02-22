935d6367e978a7264e756fe9f6773f59e96ff12c
SharkTrader90 <mrimando1990@hotmail.com>
2023-02-22 02:51:17 +0000

    UncheckedException

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int[] arr = new int[3];
+        int num = arr[3];
     }
 }
